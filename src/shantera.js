7b0ce7b6cf8afd54683501700f7af00b67f6d0d1
Jcinc2 <91334198+Jcinc2@users.noreply.github.com>
2021-09-24 10:02:49 -0500

    Update shantera.js

--- src/shantera.js
+++ src/shantera.js
@@ -1,15 +1,16 @@
 const shantera = (prefix, pushname) => {
     return `*Palabras especificas para que el bot interactue con ustedes*
-
-
 Hora del sexito
 Pongan cuties
 Fiesta del admin
 Admin party
+Viernes
 GOOOOD
 Alto temazo
 Todo bien
+Buenos dias
 Bot gay
+Gracias
 Hola
 Fua
 Corte
@@ -39,8 +40,8 @@ Un chino
 No funciona
 Boliviano
 Enano
-
 _*Ojito escribe tal y como esta en el mensaje*_
+ву ѕнαη∂υу`
 
 }
 
