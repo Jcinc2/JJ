1952c21402f46a01feb578ff50ec59f3234985e9
Jcinc2 <91334198+Jcinc2@users.noreply.github.com>
2021-09-24 10:46:08 -0500

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,6 @@
 const config = {
-        botName: 'ShanBot',
-        ownerName: 'Shan',
+        botName: 'JJ',
+        ownerName: 'AT',
         youtube: 'YOUTUBE_LINK',
         instagram: 'INSTAGRAM_LINK',
 }
